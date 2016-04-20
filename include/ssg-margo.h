765932c506f9c0017d0e530c51696bd31cfc30b9
John Jenkins <jenkins@mcs.anl.gov>
2016-04-20 15:44:51 -0500

    oops, left out a bunch of files

---
+++ include/ssg-margo.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 UChicago Argonne, LLC
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+
+#pragma once
+
+// lookup integrated into margo
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <mpi.h>
+#include <margo.h>
+
+#include "ssg.h"
+
+// a "margo-aware" version of hg_lookup - still looks up everyone in one go
+hg_return_t ssg_lookup_margo(ssg_t s, margo_instance_id mid);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * vim: ft=c sw=4 ts=4 sts=4 tw=80 expandtab
+ */
