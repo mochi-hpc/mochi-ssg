1ed7b5b136de44a6eb207e5c06565997f91b82ca
John Jenkins <jenkins@mcs.anl.gov>
2016-04-21 10:31:40 -0500

    example mpi script typo

--- examples/run-example-mpi.sh
+++ examples/run-example-mpi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run me from the top-level build dir
-mpirun -np 3 examples/ssg-example -s 0 cci+sm://localhost:3344 conf ../examples/example.conf
+mpirun -np 3 examples/ssg-example -s 0 cci+sm://localhost:3344 mpi
