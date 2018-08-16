e566e8d5dce9295e7a0f355f975f5e9d9d530368
Phil Carns <carns@mcs.anl.gov>
2018-08-16 15:40:52 -0500

    replace old regression scripts with spack version

--- tests/perf-regression/bebop/run-regression.sh
+++ tests/perf-regression/bebop/run-regression.sh
@@ -4,135 +4,67 @@
 # the LCRC, that will download, compile, and execute the ssg performance 
 # regression tests, including any dependencies
 
+# SEE README.spack.md for environment setup information!  This script will not
+#     work properly without properly configured spack environment
+
 # exit on any error
 set -e
 
-SANDBOX=/tmp/mochi-regression-$$
+module load numactl boost cmake
+
+SANDBOX=~/tmp/mochi-regression-sandbox-$$
 PREFIX=~/tmp/mochi-regression-install-$$
 JOBDIR=~/tmp/mochi-regression-job-$$
 
-export CC=icc
-export CFLAGS="-O3"
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-
-module load numactl
-module load cmake
-module load boost
-
 # scratch area to clone and build things
 mkdir $SANDBOX
+cp spack-libfabric-1.6.1.patch  $SANDBOX/
 
 # scratch area for job submission
 mkdir $JOBDIR
-
 cp margo-regression.sbatch $JOBDIR
-cp margo-regression-knl.sbatch $JOBDIR
 
 cd $SANDBOX
-git clone http://git.mpich.org/openpa.git/
-git clone https://github.com/01org/opa-psm2.git
-git clone https://github.com/ofiwg/libfabric.git
-git clone https://github.com/carns/argobots.git
-git clone https://github.com/mercury-hpc/mercury.git
-wget http://dist.schmorp.de/libev/libev-4.24.tar.gz
-tar -xvzf libev-4.24.tar.gz
-git clone https://xgitlab.cels.anl.gov/sds/margo.git
+git clone https://github.com/spack/spack.git
+git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
 git clone https://xgitlab.cels.anl.gov/sds/ssg.git
 wget http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-5.3.2.tar.gz
 tar -xvzf osu-micro-benchmarks-5.3.2.tar.gz
 git clone https://github.com/pdlfs/mercury-runner.git
+
+# set up most of the libraries in spack
+echo "=== BUILD SPACK PACKAGES AND LOAD ==="
+cd $SANDBOX/spack
+patch -p1 < ../spack-libfabric-1.6.1.patch
+. $SANDBOX/spack/share/spack/setup-env.sh
+spack repo add $SANDBOX/sds-repo
+spack uninstall -R -y argobots mercury opa-psm2 || true
+spack install --dirty ssg
+# deliberately repeat setup-env step after building modules to ensure
+#   that we pick up the right module paths
+. $SANDBOX/spack/share/spack/setup-env.sh
+spack load -r ssg
+
+export CFLAGS="-O3"
 
 # OSU MPI benchmarks
 echo "=== BUILDING OSU MICRO BENCHMARKS ==="
 cd $SANDBOX/osu-micro-benchmarks-5.3.2
 mkdir build
 cd build
-../configure --prefix=$PREFIX CC=mpiicc CXX=mpiicc
-make -j 3
-make install
-
-# argobots
-echo "=== BUILDING ARGOBOTS ==="
-cd $SANDBOX/argobots
-git checkout dev-get-dev-basic
-libtoolize
-./autogen.sh
-mkdir build
-cd build
-../configure --prefix=$PREFIX --enable-perf-opt
-make -j 3
-make install
-  
-# opa-psm2
-echo "=== BUILDING LIBFABRIC ==="
-cd $SANDBOX/opa-psm2
-make
-make DESTDIR=$PREFIX install
-
-# TODO: it doesn't seem like this should be necessary; we are specifying path
-# to lib in libfabric build.
-export LD_LIBRARY_PATH=$PREFIX/usr/lib64:$LD_LIBRARY_PATH
-
-# libfabric
-echo "=== BUILDING LIBFABRIC ==="
-cd $SANDBOX/libfabric
-./autogen.sh
-mkdir build
-cd build
-../configure --prefix=$PREFIX --enable-sockets --enable-psm2=$PREFIX/usr --enable-verbs
-make -j 3
-make install
-
-# openpa
-echo "=== BUILDING OPENPA ==="
-cd $SANDBOX/openpa
-./autogen.sh
-mkdir build
-cd build
-../configure --prefix=$PREFIX 
-make -j 3
-make install
-
-# mercury
-echo "=== BUILDING MERCURY ==="
-cd $SANDBOX/mercury
-git submodule update --init
-mkdir build
-cd build
-cmake -DNA_USE_OFI:BOOL=ON -DMERCURY_USE_BOOST_PP:BOOL=ON -DCMAKE_INSTALL_PREFIX=/$PREFIX -DBUILD_SHARED_LIBS:BOOL=ON -DMERCURY_USE_SELF_FORWARD:BOOL=ON -DNA_USE_SM:BOOL=ON ../
-make -j 3
-make install
-
-# libev
-echo "=== BUILDING LIBEV ==="
-cd $SANDBOX/libev-4.24
-mkdir build
-cd build
-../configure --prefix=$PREFIX
-make -j 3
-make install
-
-# margo
-echo "=== BUILDING MARGO ==="
-cd $SANDBOX/margo
-libtoolize
-./prepare.sh
-mkdir build
-cd build
-../configure --prefix=$PREFIX 
+../configure --prefix=$PREFIX CC=mpicc CXX=mpicxx
 make -j 3
 make install
 
 # ssg
-echo "=== BUILDING SSG ==="
+echo "=== BUILDING SSG TEST PROGRAMS ==="
 cd $SANDBOX/ssg
 libtoolize
 ./prepare.sh
 mkdir build
 cd build
-../configure --prefix=$PREFIX CC=mpiicc
+../configure --prefix=$PREFIX CC=mpicc
 make -j 3
-make install
 make tests
 
 # mercury-runner benchmark
@@ -140,7 +72,7 @@ echo "=== BUILDING MERCURY-RUNNER BENCHMARK ==="
 cd $SANDBOX/mercury-runner
 mkdir build
 cd build
-CC=mpiicc CXX=mpiicc CXXFLAGS='-D__STDC_FORMAT_MACROS' cmake -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DMPI=ON ..
+CC=mpicc CXX=mpicxx CXXFLAGS='-D__STDC_FORMAT_MACROS' cmake -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DMPI=ON ..
 make -j 3
 make install
 
@@ -152,15 +84,14 @@ cp $PREFIX/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency $JOBDIR
 cp $PREFIX/bin/mercury-runner $JOBDIR
 cd $JOBDIR
 sbatch --wait --export=ALL ./margo-regression.sbatch
-sbatch --wait --export=ALL ./margo-regression-knl.sbatch
 
 echo "=== JOB DONE, COLLECTING AND SENDING RESULTS ==="
 # gather output, strip out funny characters, mail
-cat *.out > combined.txt
-# TODO: dooesn't look like we have this on bebop, need another solution
-# dos2unix combined.txt
-mailx -s "margo-regression (bebop)" sds-commits@lists.mcs.anl.gov < combined.txt
+cat *.out > combined.$JOBID.txt
+#dos2unix combined.$JOBID.txt
+mailx -s "margo-regression (bebop)" sds-commits@lists.mcs.anl.gov < combined.$JOBID.txt
 
 cd /tmp
+spack repo rm $SANDBOX/sds-repo
 rm -rf $SANDBOX
 rm -rf $PREFIX
